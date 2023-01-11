7698d1ab4a81f5af47972018a109d2939da59f69
G702-08\GREEN <eodudchl@gmail.com>
2023-01-11 15:32:19 +0900

    회원정보수정

---
+++ miracle 최대영 회원정보수정/src/main/java/spring/controller/IdSearchController.java
@@ -0,0 +1,47 @@
+package spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import spring.dao.UserDao;
+import spring.vo.Search;
+import spring.vo.User;
+
+@Controller
+public class IdSearchController {
+	
+	private UserDao UserDao; 
+	
+	public IdSearchController(UserDao userDao) {
+		UserDao = userDao;
+	}
+	
+	@GetMapping("idSearch")
+	public String SearchIdGet() {
+		return "idSearch";
+	}
+	
+	@PostMapping("idSearchCom")
+	public String SearchId( Search search, Model model) {
+		
+		search.getUser_Name();
+		search.getUser_Tel();
+		search.getUser_Birth();
+		
+		User user_Id = UserDao.searchId(search);
+		model.addAttribute("user",user_Id);
+		
+		return "idSearchCom";
+	}
+	
+	
+	
+	
+	
+	
+
+}
