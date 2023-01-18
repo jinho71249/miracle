b3a4be4448d1a4d3c3e5bf9aeebc1363d2cdb6f8
DESKTOP-DSSTMTC\GREEN <asdf@naver.com>
2023-01-18 16:35:48 +0900

    정보공유게시판 댓글추가

---
+++ miracle 1.18 정보공유게시판댓글추가/src/main/java/spring/validator/AdoboardWriteValidator.java
@@ -0,0 +1,39 @@
+package spring.validator;
+
+import javax.servlet.jsp.tagext.ValidationMessage;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import spring.vo.Adoption;
+
+public class AdoboardWriteValidator implements Validator {
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		
+		return Adoption.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		
+		Adoption adoption=(Adoption)target;
+		
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "ado_Breed", "required"); 
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "ado_AniVaccin", "required"); 
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "ado_AniDiscovery", "required"); 
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "ado_Tel", "required"); 
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "ado_AniCareplace", "required"); 
+		
+		if(adoption.getAdo_AniAge()==0) {
+			errors.rejectValue("ado_AniAge", "ageiszero"); 
+		}
+		if(adoption.getAdo_AniWeight()==0.0) {
+			errors.rejectValue("ado_AniWeight", "weightiszero"); 
+		}
+		
+	}
+
+}
