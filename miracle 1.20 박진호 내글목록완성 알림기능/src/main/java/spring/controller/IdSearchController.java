13db06177b5d043cfa488529f82471487c4c08de
jinho71249 <jinho71249@naver.com>
2023-01-20 14:14:32 +0900

    내글목록 완성 알림기능완성

---
+++ miracle 1.20 박진호 내글목록완성 알림기능/src/main/java/spring/controller/IdSearchController.java
@@ -0,0 +1,57 @@
+package spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import spring.Exception.IdNotSearchException;
+import spring.dao.UserDao;
+import spring.service.IdSearchService;
+import spring.validator.IdSearchValidator;
+import spring.validator.RegisterRequestValidator;
+import spring.vo.RegisterRequest;
+import spring.vo.Search;
+import spring.vo.User;
+
+@Controller
+public class IdSearchController {
+	
+	private IdSearchService idSearchSvc; 
+	
+	public IdSearchController(IdSearchService idSearchSvc) {
+		this.idSearchSvc=idSearchSvc;
+	}
+	
+	@GetMapping("idSearch")
+	public String SearchIdGet(Model model) {
+		 model.addAttribute("data",new Search()); 
+		return "idSearch";
+	}
+	
+	@PostMapping("idSearchCom")
+	public String SearchId(@ModelAttribute("data") Search search, Model model,Errors errors) {
+		
+		new IdSearchValidator().validate(search, errors);
+		
+		if(errors.hasErrors()) {
+			return "idSearch";
+		}
+		try {
+			idSearchSvc.idSearch(search, model);
+			return "idSearchCom";
+		}catch (IdNotSearchException e) {
+			model.addAttribute("message","존재하지 않는 회원이거나 정보가 일치하지않습니다.");
+			return "idSearch";
+		}
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ miracle 1.20 박진호 내글목록완성 알림기능/src/main/java/spring/controller/MainController.java
@@ -0,0 +1,73 @@
+package spring.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import spring.dao.UserDao;
+import spring.vo.User;
+
+@Controller
+public class MainController {
+	private UserDao userDao;
+	
+	
+	public MainController(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	@RequestMapping("main")
+	public String gomain(HttpSession session) {
+		User curLogin=(User) session.getAttribute("curLogin");
+		if(curLogin!=null) {
+			User user=userDao.selectById(curLogin.getUser_Id());
+			session.setAttribute("curLogin", user);//db에서 데이터 다시가져와 정보저장 
+		}
+		
+		
+		return "main";
+	}
+	
+	
+	@RequestMapping("myList")
+	public String myList(@RequestParam(value="back", required = false) String back,
+			Model model, HttpSession session, HttpServletResponse response) throws IOException {
+		User curLogin=(User) session.getAttribute("curLogin");
+		model.addAttribute("user_Id", curLogin.getUser_Id());
+		if(back==null) {
+			model.addAttribute("back", "none");
+		}
+		else if(back.equals("ado")) {
+			model.addAttribute("back", "ado");
+		}
+		else if(back.equals("applGet")) {
+			model.addAttribute("back", "applGet");
+		}
+		else if(back.equals("applSent")) {
+			model.addAttribute("back", "applSent");
+		}
+		else if(back.equals("review")) {
+			model.addAttribute("back", "review");
+		}
+		else if(back.equals("info")) {
+			model.addAttribute("back", "info");
+		}
+		else if(back.equals("free")) {
+			model.addAttribute("back", "free");
+		}
+		
+		return "myList";
+	}
+	
+	
+}
+
+
+
